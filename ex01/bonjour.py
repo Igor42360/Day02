8de8b637beccda631af69612200b7918d6fa1bfa
Midnightfen <[l.grubski@it-students.fr]>
2021-06-03 16:54:54 +0200

    Ajout Day02 ex 00 01 02

---
+++ ex01/bonjour.py
@@ -0,0 +1,5 @@
+def bonjour(name): 
+  
+  print("Bonjour," + name + ", comment allez vous ?")
+
+bonjour("Thomas")
---
+++ ex02/bonjourV2.py
@@ -0,0 +1,2 @@
+name = input ("Bonjour, quel est votre prénom?\n")
+print (name + " , enchanté, moi c'est James, James Bond")
